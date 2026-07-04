108bb0f72c41b83e2825cb43c52313a70c4f7c31
aluwi0210-png <aluwi0210@gmail.com>
2026-07-05 03:57:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ button.addEventListener("click", () => {
         behavior: "smooth"
     });
 });
+const progressDot = document.querySelector(".progress-dot");
+
+window.addEventListener("scroll", () => {
+
+    const scrollTop = window.scrollY;
+
+    const docHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+
+    const percent = scrollTop / docHeight;
+
+    const lineHeight = 180;
+
+    progressDot.style.top = (percent * (lineHeight - 14)) + "px";
+
+});
